ba6bec5e5ff5ef6b095834332fe64202d8b3f927
Alejandra Coeto <a01285221@tec.mx>
2023-10-05 11:54:52 -0600

    Ultrasonic test

--- Esp8266/ultrasonic/ultrasonic.ino
+++ Esp8266/ultrasonic/ultrasonic.ino
@@ -1,6 +1,6 @@
-#define D2  5 //trigPind d2
-#define D3  4 // echoPin d3
-#define D1  0 //aqui va el led del sensor ultrasónico d1
+#define D2  4 //trigPind d2
+#define D3  0 // echoPin d3
+#define D1  5 //aqui va el led del sensor ultrasónico d1
 
 // ----- Funcion que dispara un rayo, lo apaga y espera el echo
 //       para medir distancia
@@ -37,19 +37,19 @@ void medirDistanciaCm() {
 // =================================================================
 void setup() {
    pinMode(D1,OUTPUT); // Led del sensor ultrasónico
-
-   // ----- Iniciar consola: Herramientas, Monitor serie
-   Serial.begin(9600);  
-
-   // ----- Inicializacion sensor de distancia
-   int trigPin = D2;
-   int echoPin = D3;
-   pinMode(trigPin,OUTPUT);
-   pinMode(echoPin,INPUT);
-   Serial.println("Iniciando Sensor Distancia");
+//
+//   // ----- Iniciar consola: Herramientas, Monitor serie
+   Serial.begin(115200);  
+//
+//   // ----- Inicializacion sensor de distancia
+//   int trigPin = D2;
+//   int echoPin = D3;
+   pinMode(D2,OUTPUT);
+   pinMode(D3,INPUT);
+//   Serial.println("Iniciando Sensor Distancia");
 }
 
 void loop() {
-   
+   Serial.println("HI");
    medirDistanciaCm();
 }
